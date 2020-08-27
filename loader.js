e04d83b407381648efb44aba582e9c4c0b54d9cd
周樹禾 <shuho.chou@gmail.com>
2020-08-27 10:49:19 +0800

    loaded as module to prevent naming polution

--- loader.js
+++ loader.js
@@ -1,12 +1,12 @@
-function loadScript(src, type = "") {
+function loadScript(src) {
   /* Insert the final install script to the head */
   var s = document.createElement('script');
-  if (type != "")
-    s.type = type;
+  s.type = "module";
   s.src = chrome.runtime.getURL(src);
+	/*
   s.onload = function () {
     this.remove();
-  };
+  }; */
   (document.head || document.documentElement).appendChild(s);
 }
 
