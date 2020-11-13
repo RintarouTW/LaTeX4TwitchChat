b3e3169fe9ffd141c68cecd944deed0d8be4a12f
周樹禾 <shuho.chou@gmail.com>
2020-11-13 20:35:46 +0800

    prevent to decorate the commands

--- show_note.js
+++ show_note.js
@@ -5,8 +5,13 @@ import { userOptions } from "./options.js"
 function show_note(node) {
 
   let fragments = node.getElementsByClassName('text-fragment')
-  fragments[0].setAttribute('class' , 'text-fragment l4t-note')
-  node.scrollIntoView()
+
+  if (fragments[0]) {
+    let text = fragments[0].innerText
+    if (text[0] == '!') return
+    fragments[0].setAttribute('class' , 'text-fragment l4t-note')
+    node.scrollIntoView()
+  }
 }
 
 export { show_note }
