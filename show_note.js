123c8278db98db7f48f1b21c80489e82bc1bf74e
周樹禾 <shuho.chou@gmail.com>
2020-11-14 22:27:28 +0800

    show welcome as note too.

--- show_note.js
+++ show_note.js
@@ -5,13 +5,12 @@ import { userOptions } from "./options.js"
 function show_note(node) {
 
   let fragments = node.getElementsByClassName('text-fragment')
+  if (!fragments[0]) return
 
-  if (fragments[0]) {
-    let text = fragments[0].innerText
-    if (text[0] == '!') return
-    fragments[0].setAttribute('class' , 'text-fragment l4t-note')
-    node.scrollIntoView()
-  }
+  let text = fragments[0].innerText
+  if (text[0] == '!'/* || /^Welcome! /.test(text)*/) return
+  fragments[0].setAttribute('class' , 'text-fragment l4t-note')
+  node.scrollIntoView()
 }
 
 export { show_note }
