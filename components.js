873982f9d6d205188bfd787c4ec3a27b8e5b9400
周樹禾 <shuho.chou@gmail.com>
2020-11-15 18:50:10 +0800

    make preview box absolute like a popup window

--- components.js
+++ components.js
@@ -14,7 +14,8 @@ let previewBox
 
 function createPreviewBox() {
   previewBox = document.createElement("div")
-  previewBox.setAttribute("class", "tw-align-items-center tw-overflow-hidden tw-flex")
+  // previewBox.setAttribute("class", "tw-align-items-center tw-overflow-hidden tw-flex")
+  previewBox.setAttribute("class", "l4t-preview-window")
   /* don't wrap on white space, also prevent the flex box eating the white spaces */
   previewBox.setAttribute("style", "white-space: pre; margin-left: .7em;") 
   return previewBox
@@ -93,6 +94,7 @@ function hookL4TComponents() {
   let popupButton = popupButtonForEditor()
   let chatButtonsContainer = TWChatButtonsContainer()
 
+  // chatButtonsContainer.insertBefore(popupButton, chatButtonsContainer.childNodes[1])
   chatButtonsContainer.insertBefore(previewBox, chatButtonsContainer.childNodes[1])
   chatButtonsContainer.insertBefore(popupButton, previewBox)
 }
